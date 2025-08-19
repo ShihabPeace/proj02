0e2c6acc4d22e4c6ce323f1b5fd157ffef67becc
ShihabPeace <shihabahameds4@gmail.com>
2025-08-19 21:01:16 +0600

    v2.0

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/categories', function () {
+    return view('categories')->name('categories');
+});
+Route::get('/login', function () {
+    return view('login')->name('login');
+});
+Route::get('/register', function () {
+    return view('register')->name('register');
+});
+Route::get('/single-blog', function () {
+    return view('single-blog')->name('single-blog');
+});
+Route::get('/profile', function () {
+    return view('profile')->name('profile');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard.dashboard');
 });
